e9a14ec6327198e70bf77d66fdb8121b4f1d703b
Kelangi <rajkiran3355@gmail.com>
2024-04-25 23:44:28 +0530

    chnages made in navbar component final app

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,15 +3,27 @@ import { NavLink } from "react-router-dom";
 import { GlobalContext } from "../../context";
 
 export default function Navbar() {
-  const { searchParam, setSearchParam , handleSubmit } = useContext(GlobalContext);
+  const { searchParam, setSearchParam, handleSubmit } =
+    useContext(GlobalContext);
 
   console.log(searchParam);
 
   return (
-    <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
-      <h2 className="text-2xl font-semibold">
-        <NavLink to={"/"}>FoodRecipe</NavLink>
-      </h2>
+    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6 mb-2">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <svg
+          class="fill-current h-8 w-8 mr-2"
+          width="54"
+          height="54"
+          viewBox="0 0 54 54"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+        </svg>
+        <span class="font-semibold text-xl tracking-tight">
+          <NavLink to={"/"}>FoodRecipe</NavLink>
+        </span>
+      </div>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
@@ -19,27 +31,27 @@ export default function Navbar() {
           value={searchParam}
           onChange={(event) => setSearchParam(event.target.value)}
           placeholder="Enter Items..."
-          className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
+          className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-mg shadow-red-100 focus:shadow-red-200"
         />
       </form>
-      <ul className="flex gap-5">
-        <li>
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow"></div>
+        <div className="text-white-700 ">
           <NavLink
             to={"/"}
-            className="text-black hover:text-gray-700 duration-300"
+            className="text-black hover:text-grey-500 duration-300 m-2 text-red-600 "
           >
             Home
           </NavLink>
-        </li>
-        <li>
+
           <NavLink
             to={"/favorites"}
-            className="text-black hover:text-gray-700 duration-300"
+            className="text-black hover:text-grey-300 duration-300 text-red-600"
           >
-            favorites
+            Favorites
           </NavLink>
-        </li>
-      </ul>
+        </div>
+      </div>
     </nav>
   );
 }
